26c5731379e2fcd8e623bb089131031e3c2bd7be
Antonio Pérez <aperez@skarcha.com>
2014-02-11 00:12:54 +0100

    Clean up

--- week 3/Permissions/SourceFiles/Skeletons/PermissionsLab/src/course/labs/permissionslab/BookmarksActivity.java
+++ week 3/Permissions/SourceFiles/Skeletons/PermissionsLab/src/course/labs/permissionslab/BookmarksActivity.java
@@ -78,8 +78,8 @@ public class BookmarksActivity extends Activity {
 
 		Log.i(TAG, "Entered startGoToDangerousActivity()");
 
-		// TODO - Start the GoToDangerousActivity
-		
+		Intent intentGoToDangerous = new Intent(this, GoToDangerousActivity.class);
+		startActivity(intentGoToDangerous);
 
 	}
 
